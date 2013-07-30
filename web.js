486d81f828b771011d19de01d8253a9df17e6028
bcooktulane <bcooktulane@gmail.com>
2013-07-30 21:09:30 +0000

    changed web.js to read about.html

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
+var about_html = 'about.html';
 
 var app = express.createServer(express.logger());
 
@@ -8,6 +9,11 @@ app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
     response.send(html);
 });
+
+app.get('/about', function(req, res) {
+    res.send(fs.readFileSync(about_html).toString());
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
