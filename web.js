18bc0bedfa896cdc4beec61d5a3f15ceb9e1c654
bcooktulane <bcooktulane@gmail.com>
2013-08-01 18:23:54 +0000

    changed web.js for jetstrapproject

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
-var about_html = 'about.html';
+var htmlfile = "jetstrap-project/home.html";
+var about_html = 'jetstrap-project/about.html';
+var contact_html = 'jetstrap-project/contact.html';
 
 var app = express.createServer(express.logger());
+app.use('/assets', express.static('jetstrap-project/assets'));
 
 app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
@@ -14,6 +16,9 @@ app.get('/about', function(req, res) {
     res.send(fs.readFileSync(about_html).toString());
 });
 
+app.get('/contact', function(reqq, ress) {
+    ress.send(fs.readFileSync(contact_html).toString());
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
